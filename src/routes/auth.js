318213488fa20ec4124c263261fbc7152d5a2765
Manuel Moya <manuel.moyavadillo2@gmail.com>
2023-03-07 12:46:59 +0100

    docs: auth endpoints commented

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,19 +1,32 @@
 const {signup, login} = require('../controllers/auth')
 const router = require('express').Router()
 
+/**
+ * *SIGN UP ENDPOINT*
+ * *localhost:8000/auth/signup*
+ * @param {Request} request
+ * @param {Response} response
+ * @returns {String}
+ */
 router.post('/signup', async (request, response) => {
     try {
-        const { email, password} = request.body
+        const { email, password } = request.body;
         if (!email || !password) {
-            response.status(502).json('Incorrect data')
+            response.status(502).json('Email or Password not found');
         } 
-        const token = await signup({email, password})
-        response.status(200).json(token)
+        const token = await signup({email, password});
+        response.status(200).json(token);
     } catch (error) {
-        response.status(500).json(error.message)
+        response.status(500).json('Error at Sign up' + error.message);
     }
 })
-
+/**
+ * *LOGIN UP ENDPOINT*
+ * *localhost:8000/auth/login*
+ * @param {Request} request
+ * @param {Response} response
+ * @returns {String}
+ */
 router.post('/login', async (request, response) => {
     try {
         const {email, password} = request.body
