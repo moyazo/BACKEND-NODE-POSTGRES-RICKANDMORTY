e86cc57645434fd216474d736942121b8a88fdec
Manuel Moya <manuel.moyavadillo2@gmail.com>
2023-03-08 19:52:05 +0100

    feat: relation user character

--- src/models/character.js
+++ src/models/character.js
@@ -8,7 +8,12 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            // character and episode   N:M
+            models.Character.belongsToMany(models.User, {
+                through: 'usercharacter',
+                as: 'favoritedBy',
+                foreignKey: 'character_id',
+                onDelete: 'cascade',
+            })
         }
     }
     Character.init(
--- src/models/user.js
+++ src/models/user.js
@@ -8,7 +8,12 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            // define association here
+            User.belongsToMany(models.Character, {
+                through: 'usercharacter',
+                as: 'favorites',
+                foreignKey: 'user_id',
+                onDelete: 'cascade',
+            })
         }
     }
     User.init(
